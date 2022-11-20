208bf2054031f5c4a5771d8bb6436e6a60f5fe24
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-20 22:45:31 +0900

    feat: 추천시스템 서버 구축

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from recommend import find_sim_books
+import json
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route('/')
+def root():
+    return 'Server is running'
+
+
+# GET: 책 한권의 isbn을 파라미터로 보내면 유사도 높은 책 20개 반횐
+# 책 상세페이지 에서 사용할 것
+# 예시: http://127.0.0.1:5000/recommend/one?isbn=9791156759270
+@app.route('/recommend/one', methods=['GET'])
+def recommend_one():
+    params = request.args.get('isbn')
+    result = find_sim_books([int(params)])
+
+    return jsonify({
+        'message': 'success',
+        'data': result
+    })
+
+# POST: 여러 책의 isbn 값을 넘기면 책 추천 받음
+# 유저가 등록한 책들의 리스트를 인풋으로 받을 것
+# 예시: http://127.0.0.1:5000/recommend/multi
+
+
+@app.route('/recommend/multi', methods=['POST'])
+def recommend_multi():
+    body = json.loads(request.get_data(), encoding='utf-8')
+    body = list(map(int, body))
+    result = find_sim_books(body)
+
+    return jsonify({
+        'message': 'success',
+        'data': result
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ recommend.py
@@ -0,0 +1,50 @@
+from ast import literal_eval
+import pandas as pd
+import numpy as np
+import warnings
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import random
+
+warnings.filterwarnings(action='ignore')
+
+books = pd.read_csv('./books_mod2.csv', encoding='utf-8')
+books = books.replace({np.nan: 'none'})
+books_df_nax = books.dropna(axis='rows')
+tfidf = TfidfVectorizer(stop_words='english')
+title_vect2 = tfidf.fit_transform(books_df_nax['title'])
+
+
+def find_sim_books(U):
+
+    def userbooks(A):
+        l = len(A)
+        n = random.randrange(0, l)
+        return A[n]
+
+    B = userbooks(U)
+
+    def sim_idx_with_A(df, isbn):
+
+        print(df[df['isbn'] == isbn]['tit_cluster'])
+        n = int(df[df['isbn'] == isbn]['tit_cluster'])
+        same_clu_books_df = df[df['tit_cluster'] == n]
+        sen = same_clu_books_df['content']
+        clu_tfidf = tfidf.fit_transform(sen)
+        clu_des_sim = cosine_similarity(clu_tfidf, clu_tfidf)
+        idx = np.where(same_clu_books_df['isbn'] == B)[0][0]
+        books_sim_vect = clu_des_sim[idx:idx+1]
+        books_des_sim_idx = books_sim_vect.argsort()[::-1]
+
+        return books_des_sim_idx
+    sim_books_idx = sim_idx_with_A(books_df_nax, B)
+    top_n = 20
+    top_sim_idx = sim_books_idx[0][:top_n]
+    top_sim_idx = top_sim_idx.reshape(-1,)
+
+    sim_books = books_df_nax.iloc[top_sim_idx]
+    outputs = sim_books[['title', 'author', 'content',
+                         'thumbnail', 'publisher', 'kdc', 'price', 'isbn']]
+    outputs_dic = outputs.to_dict('records')
+
+    return outputs_dic
